4f32bf897bbe9dc37217168442fd69eb0d032d93
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-01-07 22:30:28 +0530

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,12 @@
+import math
+def isprime(num):
+    if(num%2 == 0):
+        return "no"
+    for i in range(3,int(math.sqrt(num)+1)):
+        if(num% i == 0):
+            return "no"
+    else:
+        return "yes"
+
+num = int(input(""))
+print(isprime(num))
