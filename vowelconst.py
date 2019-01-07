5768121aca21965640690fa8f214eb37ee140af5
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-01-07 09:32:22 +0000

    Update vowelconst.py

--- vowelconst.py
+++ vowelconst.py
@@ -3,6 +3,6 @@ test = val.lower()
 if(test == 'a' or test == 'e' or test == 'i' or test == 'o' or test == 'u'):
     print("Vowel")
 elif(ord(test)>=97 and ord(test)<=122):
-    print("Constant")
+    print("Consonant")
 else:
     print("invalid")
