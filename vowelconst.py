b69392ccc465b784391f1b5c144fa98e28c59246
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-01-07 09:30:27 +0000

    Create vowelconst.py

---
+++ vowelconst.py
@@ -0,0 +1,8 @@
+val = input("")
+test = val.lower()
+if(test == 'a' or test == 'e' or test == 'i' or test == 'o' or test == 'u'):
+    print("Vowel")
+elif(ord(test)>=97 and ord(test)<=122):
+    print("Constant")
+else:
+    print("invalid")
