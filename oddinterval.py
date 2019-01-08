5c62c9f10b39c71e9d52e8cf0466f2827870dbfa
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-01-08 12:21:04 +0530

    Update oddinterval.py

--- oddinterval.py
+++ oddinterval.py
@@ -1,6 +1,10 @@
 a,b = map(int,input().split())
-
+c = 0
 for i in range(a+1,b):
-    if(i%2 != 0):
-        print(i,end = " ")
+    if(i%2 1= 0):
+        if(c!=0):
+            print(" ",end ="")
+        print(i,end = "")
+        c+=1
+    
     
