43602f27b6599465eecf2e645d16033bb81ca5d2
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-01-08 12:22:24 +0530

    Update eveninterval.py

--- eveninterval.py
+++ eveninterval.py
@@ -1,6 +1,11 @@
 a,b = map(int,input().split())
-
+c = 0
 for i in range(a+1,b):
     if(i%2 == 0):
-        print(i,end = " ")
+        if(c!=0):
+            print(" ",end ="")
+        print(i,end = "")
+        c+=1
+    
+
     
