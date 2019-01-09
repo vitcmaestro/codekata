256add41147e4b6c59fa966f515d59996c9b9881
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-01-09 16:19:58 +0530

    Create mergesort.py

---
+++ mergesort.py
@@ -0,0 +1,39 @@
+def mergeSort(alist):
+
+   if len(alist)>1:
+       mid = len(alist)//2
+       lefthalf = alist[:mid]
+       righthalf = alist[mid:]
+
+       #recursion
+       mergeSort(lefthalf)
+       mergeSort(righthalf)
+
+       i=0
+       j=0
+       k=0
+
+       while i < len(lefthalf) and j < len(righthalf):
+           if lefthalf[i] < righthalf[j]:
+               alist[k]=lefthalf[i]
+               i=i+1
+           else:
+               alist[k]=righthalf[j]
+               j=j+1
+           k=k+1
+
+       while i < len(lefthalf):
+           alist[k]=lefthalf[i]
+           i=i+1
+           k=k+1
+
+       while j < len(righthalf):
+           alist[k]=righthalf[j]
+           j=j+1
+           k=k+1
+
+n = int(input(""))
+arr = list(map(int,input().split()))
+mergeSort(arr)
+for i in range(len(arr)):
+    print(arr[i],end=" ")
